88ea410e80f8b4b8b9fbb1e167093134c89994b3
Carson Cooley <cbcooley@comcast.net>
2021-07-02 19:25:53 -0400

    Initial Commit

---
+++ Math-a-Mole-1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math-a-Mole-1.0
+//
+//  Created by Carson Cooley on 7/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
